dbe67c4afec0733eaae1c3bd4a7424b28a792eb5
vlad17451 <vlad17451@gmail.com>
2021-08-20 21:33:54 +0700

    requires + update chain status

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,31 @@
 const hre = require("hardhat");
+const BigNumber = require("bignumber.js")
+BigNumber.config({ EXPONENTIAL_AT: 60 });
+
+const delay = async (time) => {
+  return new Promise((resolve) => {
+    setInterval(() => {
+      resolve()
+    }, time)
+  })
+}
 
 async function main() {
   require('dotenv').config();
-  const token = await hre.ethers.getContractFactory("AcademyToken");
-  const tokenInst = await token.deploy();
+  const Token = await hre.ethers.getContractFactory("AcademyToken");
+  const tokenInst = await Token.deploy("Academy Token 2", "ACDM2");
   await tokenInst.deployed();
   console.log("token address:", tokenInst.address);
+
+  await delay(5000);
+
+  // const tokenInst = await Token.attach('0x174A683F9caCDB6Da6B9a6a637204971437bB5FA')
+
+  await tokenInst.mint(
+    '0xBC6ae91F55af580B4C0E8c32D7910d00D3dbe54d',
+    new BigNumber('10000').shiftedBy(18).toString()
+  )
+
 }
 
 main()
